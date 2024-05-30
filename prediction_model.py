eac236591303ae47e4a540e24e8be3945e303250
ALargeCoffee <84868705+ALargeCoffee@users.noreply.github.com>
2024-05-30 16:19:12 -0400

    Creating initial model structure

--- prediction_model.py
+++ prediction_model.py
@@ -1,4 +1,3 @@
-import torch
 from torch import nn
 
 # Class for the predictor model
@@ -6,6 +5,19 @@ class MVPPredictorModel(nn.Module):
 
     def __init__(self):
         super().__init__()
+        self.model_sequence = nn.Sequential(
+            nn.Linear(15, 30),
+            nn.ReLU(),
+            nn.LayerNorm(30),
+            nn.Linear(30, 60),
+            nn.ReLU(),
+            nn.LayerNorm(60),
+            nn.Linear(60, 30),
+            nn.ReLU(),
+            nn.LayerNorm(30),
+            nn.Linear(30, 15),
+            nn.Softmax()
+        )
 
-    def forward(input):
-        return -1
+    def forward(self, input):
+        return self.model_sequence(input)
